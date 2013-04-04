924bc1fa1d4568fc4dd855419cbc5028b368b9c9
Kevin Tuhumury <kevin.tuhumury@gmail.com>
2013-04-05 01:53:07 +0200

    Fixed indenting.

--- jobs/klimato.rb
+++ jobs/klimato.rb
@@ -13,14 +13,14 @@ format = "c"
 query  = URI::encode "select * from weather.forecast WHERE woeid=#{woeid} and u='#{format}'&format=json"
 
 SCHEDULER.every "15m", :first_in => 0 do |job|
-  http 	   = Net::HTTP.new "query.yahooapis.com"
+  http     = Net::HTTP.new "query.yahooapis.com"
   request  = http.request Net::HTTP::Get.new("/v1/public/yql?q=#{query}")
   response = JSON.parse request.body
   results  = response["query"]["results"]
 
   if results
- 	condition = results["channel"]["item"]["condition"]
- 	location  = results["channel"]["location"]
- 	send_event "klimato", { location: "#{location['city']}", temperature: "#{condition['temp']}", code: "#{condition['code']}" }
+    condition = results["channel"]["item"]["condition"]
+    location  = results["channel"]["location"]
+    send_event "klimato", { location: "#{location['city']}", temperature: "#{condition['temp']}", code: "#{condition['code']}" }
   end
 end
