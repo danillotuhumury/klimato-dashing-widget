5810c6c382a448966cc455918c7e6f2fd945fca2
Kevin Tuhumury <kevin.tuhumury@gmail.com>
2013-04-05 09:10:31 +0200

    There's no need for interpolation, since we're only using variables.

--- jobs/klimato.rb
+++ jobs/klimato.rb
@@ -21,6 +21,6 @@ SCHEDULER.every "15m", :first_in => 0 do |job|
   if results
     condition = results["channel"]["item"]["condition"]
     location  = results["channel"]["location"]
-    send_event "klimato", { location: "#{location['city']}", temperature: "#{condition['temp']}", code: "#{condition['code']}", format: format }
+    send_event "klimato", { location: location["city"], temperature: condition["temp"], code: condition["code"], format: format }
   end
 end
